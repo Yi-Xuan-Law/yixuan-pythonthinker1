ea6933732f9096d7597f38eb323595157cbf620f
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-02-16 03:02:13 +0000

    Feb 16, 2025, 3:02 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,48 @@
-print("Hello from lesson 4")
+
+# print("Hello from lesson")
+      
+# red_plate = 1
+# blue_plate = 2
+# green_plate = 3
+# red_plate = 1
+# blue_plate = 2
+# green_plate = 3
+
+# red_plate_num = 3
+# blue_plate_num = 5
+# green_plate_num = 4
+
+# total_price = (red_plate * red_plate_num)+(blue_plate * blue_plate_num)+(green_plate * green_plate_num)
+
+# print(total_price)
+
+
+
+# name = input("What is you name?")
+# print(name)
+
+# fav_colour = input("What is your favourite colour?")
+# print(fav_colour)
+
+# age = input("What is your age?")
+# print(age)
+
+
+
+
+# name = input("What is your name?")
+# print("Hi, " + name + ".")
+
+# by + "!" )
+
+# vaca = input("What is your dream vacation destination?")
+# print(" I would love to vist " + vaca)
+# hobby = input("What is your hobby?" )
+# print("I enjoy " + hobby
+
+
+
+age = input("What is you age?")
+age = int(age)
+age = age + 1
+print("next year you will be " + str(age) + "years old")
