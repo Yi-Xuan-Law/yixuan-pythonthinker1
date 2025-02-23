94095f407c55bdb643df69ca79d35e3825f6d66d
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-02-23 04:28:35 +0000

    Feb 23, 2025, 4:28 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,18 +1,18 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = total / 3
+# average_score = total / 3
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + str(average_score))
+# print("Average score for " + student_name + " is: " + str(average_score))
 
-for number in range(1, 11):
-    print(number)
+# for number in range(1, 11):
+#     print(number)
 
