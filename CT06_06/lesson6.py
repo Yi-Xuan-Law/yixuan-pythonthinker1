722cb00dac193b99944262809edbc1e9782658a0
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-02-23 03:45:42 +0000

    Feb 23, 2025, 3:45 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,2 +1,18 @@
-print("Hello from lesson 6")
+# print("Hello from lesson 6")
 
+# **Task 1a**:
+for i in range(3):
+    print("Hello, World!")
+
+# **Task 1b**:
+for i in range(5):
+    print(i)
+
+# **Task 1c**:
+print("Hello, World!")
+
+# **Task 1d**:
+i = 5
+    
+# **Task 1e**:
+print ("Hello, World!"
