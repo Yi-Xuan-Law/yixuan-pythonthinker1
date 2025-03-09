0e52ecbfc4badf393a936cd72f634b193bce1ef7
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-03-09 03:44:10 +0000

    Mar 9, 2025, 3:44 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -2,7 +2,7 @@
 # print("nice to meet you, " + name)  #nice to meet you, person's name
 
 
-start=input=("What is your start number?")
+start=input("What is your start number?")
 stop=input("What is your stop number?")
 increament=input("What is the increament?")
 
