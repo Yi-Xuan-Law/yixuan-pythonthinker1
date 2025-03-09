a9a79fae34ba53013ab792cd1da033aad2e11c36
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-03-09 03:36:10 +0000

    Mar 9, 2025, 3:36 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -5,3 +5,5 @@ print("nice to meet you, " + name)  #nice to meet you, person's name
 start=int(("What is your start number?"))
 stop=int(("What is your stop number?"))
 increament=int(("What is the increament?"))
+
+print()
