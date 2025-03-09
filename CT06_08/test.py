ec3b64380796d3ec8a1022eb498c91cd7a72f46d
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-03-09 04:02:58 +0000

    Mar 9, 2025, 4:02 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -2,12 +2,12 @@
 # print("nice to meet you, " + name)  #nice to meet you, person's name
 
 
-start=int(input("What is your start number?"))   #ask user what is the start number
-stop=int(input("What is your stop number?"))      # ask user what is their stop number
-increament=int(input("What is the increament?"))   #ask user what is their increament
+# start=int(input("What is your start number?"))   #ask user what is the start number
+# stop=int(input("What is your stop number?"))      # ask user what is their stop number
+# increament=int(input("What is the increament?"))   #ask user what is their increament
 
-for i in range(start, stop, increament):               #print
-    print(i)
+# for i in range(start, stop, increament):               #print
+#     print(i)
 
 
 age=11
