70e67d9aadeaf517612f729aad9597ea5880b0e8
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-03-16 03:19:33 +0000

    Mar 16, 2025, 3:19 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -52,4 +52,4 @@
 
 sum=0
 for number in range(1, 6):
-    sum=sum+int(input("What is the score of the student "+ number"?"))
+    sum=sum+int(input("What is the score of the student "+ number + "?"))
