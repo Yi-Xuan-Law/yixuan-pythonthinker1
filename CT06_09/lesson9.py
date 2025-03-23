28eb1afbb1db5d4553a1545e8877023c0257ba69
Yi-Xuan-Law <lawyixuanlyx@gmail.com>
2025-03-23 03:55:56 +0000

    Mar 23, 2025, 3:55 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,19 +1,20 @@
 print("Hello from lesson 9")
 
-import random
+# import random
 
-num1 = random.randint(1, 6)
-num2 = random.randint(1, 6)
-num3 = random.randint(1, 6)
+# num1 = random.randint(1, 6)
+# num2 = random.randint(1, 6)
+# num3 = random.randint(1, 6)
 
-print("1st number: " + str(num1) )
-print("2nd number: " + str(num2) )
-print("3rd number: " + str(num3) )
+# print("1st number: " + str(num1) )
+# print("2nd number: " + str(num2) )
+# print("3rd number: " + str(num3) )
 
-num1_even = num1 % 2 == 0
-num2_even = num2 % 2 == 0
-num3_even = num3 % 2 == 0
+# num1_even = num1 % 2 == 0
+# num2_even = num2 % 2 == 0
+# num3_even = num3 % 2 == 0
 
-all_even_odd = num1_even = num2_even = num3_even
+# all_even_odd = num1_even = num2_even = num3_even
 
-print(all_even_odd)
+# print(all_even_odd)
+
